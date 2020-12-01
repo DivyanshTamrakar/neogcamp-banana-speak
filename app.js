57cde238ab39ee11759508677c734df8bc54df0e
DivyanshTamrakar <tamrakardivyansh189@gmail.com>
2020-12-01 22:25:55 +0530

    taking input and console

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
 var btntranslate = document.querySelector("#btn-translate");
+var inputtxt =  document.querySelector("#input-txt");
+console.log(inputtxt)
 btntranslate.addEventListener("click", click);
 
 function click(){
     console.log("clicked");
+    if(inputtxt.value === "")
+    {
+        alert("you have to write something");
+    }
+    else
+    {
+        console.log("input ",inputtxt.value);
+    }
 }
