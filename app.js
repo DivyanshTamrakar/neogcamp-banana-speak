8c658e49cd5cb30370068ce9a4934af18b150225
DivyanshTamrakar <tamrakardivyansh189@gmail.com>
2020-12-01 19:06:37 +0530

    import javascript

---
+++ app.js
@@ -0,0 +1,11 @@
+// console.log("hello world");
+// console.log("My name is divyansh tamrakar")
+
+// input 
+var userinput = prompt("Tell me your name ")
+
+//processing 
+var welcome  = "this script is " + userinput;
+
+// output 
+alert(welcome);
