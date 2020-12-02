1091b943074281a6208049bdaf91adc94d7fca65
DivyanshTamrakar <tamrakardivyansh189@gmail.com>
2020-12-03 02:24:12 +0530

    all things done

--- app.js
+++ app.js
@@ -1,16 +1,38 @@
 var btntranslate = document.querySelector("#btn-translate");
-var inputtxt =  document.querySelector("#input-txt");
+var inputtxt = document.querySelector("#input-txt");
 var outputtxt = document.querySelector("#output");
 console.log(outputtxt);
 btntranslate.addEventListener("click", click);
 
-function click(){
 
-    outputtxt.innerText = "sfsfsdfsd" + inputtxt.value  ;
-  
+var serviceurl = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
+// var serviceurl = "https://api.funtranslations.com/translate/minion.json";
+
+
+function getTranslation(text) {
+    var fullurl = serviceurl + "?" + "text=" + text;
+    console.log(fullurl);
+    return fullurl;
 }
 
-// live exericse no 4 lesson 4
+function errorHandler(error) {
+    console.log("error: " + error);
+    alert("Something went wrong with the server , please try again after some time");
+}
+
+function click() {
+
+    inputtxt.value = ''
+
+     
+    fetch(getTranslation(inputtxt.value)).then(response => response.json()).then(json => {
+        console.log(json);
+        outputtxt.innerText = json.contents.translated;
+    }).catch(errorHandler);
+
+}
+
+// live exericse no 4 lesson 4  
 // "textarea"
 // ".btn-primary"
 // "input-btn"
